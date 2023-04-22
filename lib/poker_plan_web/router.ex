f7563f38a2ea4ed003a147d646698e4357f10742
Udalov Igor <i.udalov@gmail.com>
2023-04-22 11:37:32 +0300

    First example

--- lib/poker_plan_web/router.ex
+++ lib/poker_plan_web/router.ex
@@ -28,7 +28,7 @@ defmodule PokerPlanWeb.Router do
   scope "/", PokerPlanWeb do
     pipe_through :browser
 
-    live "/counter", CounterLive
+    # live "/counter", CounterLive
     get "/", PageController, :home
   end
 
